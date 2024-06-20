1f53eb045148717553c6f0573a094c22914a7b1a
Andreas.Marginean <andreas.marginean96@gmail.com>
2024-06-20 14:46:00 +0200

    controller + main

---
+++ src/main/java/org/chatapp/SpringBootChatApplication.java
@@ -0,0 +1,14 @@
+package org.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChatApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                SpringBootChatApplication.class , args);
+    }
+}
